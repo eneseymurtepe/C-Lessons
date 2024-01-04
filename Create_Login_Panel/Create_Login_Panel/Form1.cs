0338960fafd434666c61d3118d11b5194892a049
Enes EYMÜRTEPE <eymurtepeenes@gmail.com>
2024-01-04 09:13:23 +0300

    Create Login Panel

---
+++ Create_Login_Panel/Create_Login_Panel/Form1.cs
@@ -0,0 +1,15 @@
+namespace Create_Login_Panel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Giriþ Baþarýlý");
+        }
+    }
+}
